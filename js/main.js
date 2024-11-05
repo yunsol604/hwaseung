4d59e3b6861abbe467043c7052130c87b179a483
yunsol604 <chldbsthf12@gmail.com>
2024-11-05 15:34:20 +0900

    241105 commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-/* header on */
-// $('header').hover(function() {
-//    $('header').toggleClass('on');})
 
-$(window).scroll(function() { //스크롤링이 될 때마다
+//*** Header
+$(window).scroll(function() { //스크롤되면 header.on
    if($(window).scrollTop() >= 660) {
       $('header').addClass('on')
    }
@@ -12,7 +10,6 @@ $(window).scroll(function() { //스크롤링이 될 때마다
    $('.family_wrap').hide()
 });
 
-/* Header */
 $('.header_left #gnb>li').hover(function() { //over
    $('.header_left .allmenu_wrap').stop().slideDown();
    $('header').addClass('on')
@@ -37,14 +34,13 @@ $('.header_left #gnb li').hover(function() {
    $('header')
 })
 
-/* Footer - Family Site */
+//*** Footer - Family Site
 $('.familysite_wrap').click(function() {
    $('.family_wrap').toggle()
 });
 
-/* Main-manage-Swiper*/
+//*** Main-manage-Swiper
 const mainSwiper = new Swiper ('.swiper_content', {
-   // effect:
    loop: false,
    autoplay: {
       delay: 3000,
@@ -75,3 +71,25 @@ $('.swiper_content .auto-stop').click(function() {
    $('.swiper_content .auto-play').show();
       mainSwiper.autoplay.stop();
 })
+
+//*** 스크롤 시 ht_box 소제목 아래에서 위로 올라오는 fade 효과
+$('.ht_box').css({
+   opacity: 0,
+   transform: 'translateY(30px)',
+   transition: 'opacity 0.6s ease, transform 0.6s ease'
+});
+$(window).on('scroll', function() {
+   $('.ht_box').each(function() {
+      let windowHeight = $(window).height();
+      let scrollTop = $(window).scrollTop();
+      let offsetTop = $(this).offset().top;
+
+      //화면의 30% 지점에서 보이도록
+      if (scrollTop + windowHeight * 0.7 > offsetTop) {
+         $(this).css({
+            opacity: 1,
+            transform: 'translateY(0)' //원래 위치로 이동
+         });
+      }
+   });
+});
